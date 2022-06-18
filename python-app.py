83f5a1dbd1fb426e6b84a6259769bf169c83577c
ocpbuild <85306279+ocpbuild@users.noreply.github.com>
2022-06-18 07:56:04 -0700

    Update python-app.py

--- python-app.py
+++ python-app.py
@@ -7,47 +7,29 @@ import os
 import re
 
 while True:
-  print("Hello World")
-  time.sleep(2)
-  os.chdir("/targetdir")
+    print("Hello from IBM zCX for OpenShift")
+    time.sleep(2)
 
-  cmd = 'echo create a new file with string deepti > myfile'
-  print(cmd)
-  result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    cmd = 'whoami'
+    print(cmd)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
 
-  sys.stderr.write(result.stderr)
-  sys.stdout.write(result.stdout)
-  time.sleep(6)
-  if result.returncode != 0:
-    exit(1)
-
-  cmd = 'echo append second line to text file >> myfile'
-  print(cmd)
-  result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-
-  sys.stderr.write(result.stderr)
-  sys.stdout.write(result.stdout)
-  time.sleep(6)
-  if result.returncode != 0:
-    exit(1)
-  
-  cmd = 'cat myfile'
-  print(cmd)
-  result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-  if not re.search("deepti", result.stdout):
-    exit(1)
-  if not re.search("append", result.stdout):
-    exit(1)
-  else:
     sys.stderr.write(result.stderr)
     sys.stdout.write(result.stdout)
-  time.sleep(6)
+    time.sleep(6)
+    if result.returncode != 0:
+        exit(1)
 
-  cmd = 'rm myfile'
-  print(cmd)
-  result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-  sys.stderr.write(result.stderr)
-  sys.stdout.write(result.stdout)
-  time.sleep(6)
-  if result.returncode != 0:
-    exit(1)
+    cmd = 'id'
+    print(cmd)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+
+    sys.stderr.write(result.stderr)
+    sys.stdout.write(result.stdout)
+    time.sleep(6)
+    if result.returncode != 0:
+        exit(1)
+
+    cmd = 'Python app is running!'
+    print(cmd)
+    time.sleep(6)
